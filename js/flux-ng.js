e25c942128ffd66fe472c1401c98138046722e75
Max Kaye <max.kaye@industrieit.com>
2015-11-08 11:25:58 +1100

    Dynamically get membership count -- needs watching and testing

--- js/flux-ng.js
+++ js/flux-ng.js
@@ -1,14 +1,23 @@
 var fluxApp = angular.module('fluxApp', []);
 
-fluxApp.controller('FluxController', function($scope, $log, $rootScope){
+fluxApp.controller('FluxController', function($scope, $log, $rootScope, $http){
   $rootScope._ = _;
 
   var flux = this;
 
-  flux.members = 126;
+  flux.members = 125;
   flux.incrementMembers = function(){
     flux.members += 1;
   }
+  flux.loadMembers = function(){
+    $http.get('http://cors.io/?u=https://docs.google.com/spreadsheets/d/1oODt6m__XMuTT69nekt98ZY6Cl_cWpaQenMrjmLqDmo/pubchart?oid=53928827&format=interactive')
+      .success(function(data){
+        var pattern = /\d+/g;
+        var numbers = data.match(pattern);
+        flux.members = numbers[numbers.length - 20];
+      })
+  }
+  flux.loadMembers();
 
   flux._showThanks = false;
   flux.showThanks = function(){

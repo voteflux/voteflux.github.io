e0a56118f5b8b13ad457dc5cdea69e842adf6001
Max Kaye <max.kaye@industrieit.com>
2015-11-08 11:38:05 +1100

    add scrolling on submit

--- js/flux-ng.js
+++ js/flux-ng.js
@@ -10,14 +10,22 @@ fluxApp.controller('FluxController', function($scope, $log, $rootScope, $http){
     flux.members += 1;
   }
   flux.loadMembers = function(){
+    // use cors.io proxy to get around cors
     $http.get('http://cors.io/?u=https://docs.google.com/spreadsheets/d/1oODt6m__XMuTT69nekt98ZY6Cl_cWpaQenMrjmLqDmo/pubchart?oid=53928827&format=interactive')
       .success(function(data){
         var pattern = /\d+/g;
         var numbers = data.match(pattern);
-        flux.members = numbers[numbers.length - 20];
+        // choose whatever is 20th from the end, should be the right number...
+        flux.members = parseInt(numbers[numbers.length - 20], 10);
+        // $log.log(numbers);
       })
   }
   flux.loadMembers();
+
+  flux.memberSubmit = function(){
+    flux.showThanks();
+    smoothScroll.animateScroll(null, '#membership');
+  }
 
   flux._showThanks = false;
   flux.showThanks = function(){

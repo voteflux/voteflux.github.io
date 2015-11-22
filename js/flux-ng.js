f230fdcdcc143be3709a53971ffe408982c1fa37
Max Kaye <m@xk.io>
2015-11-22 17:30:10 +1100

    Add submit to api!

--- js/flux-ng.js
+++ js/flux-ng.js
@@ -6,9 +6,16 @@ fluxApp.controller('FluxController', function($scope, $log, $rootScope, $http){
   $rootScope._ = _;
 
   var flux = this;
-  
+
+  if (document.location.hostname == 'localhost'){
+    flux.debug = true;
+  }
+
   flux.api = function(path){
-	  return "https://api.voteflux.org/" + path;
+    if (flux.debug){
+      return "http://localhost:5000/" + path;
+    }
+    return "https://api.voteflux.org/" + path;
   };
 
   flux.members = 125;
@@ -27,6 +34,39 @@ fluxApp.controller('FluxController', function($scope, $log, $rootScope, $http){
   flux.memberSubmit = function() {
     flux.showThanks();
     smoothScroll.animateScroll(null, '#membership');
+
+    var getEntry = function(name){
+      var obj = $("[name='" + name + "']");
+      $log.log(obj);
+      return obj.val();
+    };
+
+    var dob = new Date();
+    dob.setUTCDate(getEntry('entry.1115890700_day'));
+    dob.setUTCMonth(parseInt(getEntry('entry.1115890700_month')) - 1);
+    dob.setUTCFullYear(getEntry('entry.1115890700_year'));
+    var to_send = {
+      'name': getEntry('entry.1069132858'),
+      'valid_regions': getEntry('entry.485675243') === "Yes" ? ['AUS'] : [],
+      'email': getEntry('entry.1201109565'),
+      'address': getEntry('entry.1799101669'),
+      'dob': dob,
+      'contact_number': getEntry('entry.134473684'),
+      'referred_by': getEntry('entry.279410956'),
+      'member_comment': getEntry('entry.1861406557')
+    };
+
+    $log.log(to_send);
+
+    $http.post(flux.api('register/all_at_once'), to_send).then(
+        function(data){
+          $http.post(flux.api('stats/all_at_once'));
+        }, function(error){
+          $http.post(flux.api('error/all_at_once'), error);
+          toastr.error(error.data.error_args);
+          $log.log(error.data);
+        }
+    );
   };
 
   flux._showThanks = false;
